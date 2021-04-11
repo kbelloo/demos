bab8b15acd51ed2d2f90e411f312777d99c46fb0
Khalid Bello <khalidbello279@gmail.com>
2021-04-11 11:30:31 +0100

    adding data to the page

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-d3.select('body').append('p').text('Hello D3');
+
+let margin = {top: 30, right: 30, bottom: 30, left: 30},
+    width = 960 - margin.left -margin.right, 
+    height = 500 - margin.top - margin.bottom;
+
+
+let svg = d3.select('#viz')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+
+
+  d3.csv('sales.csv', (d) => {
+
+    d.sales = +d.sales;
+    return d;
+
+  }).then((results) => {
+
+    console.log(results);
+
+  }).catch((error) => {
+      throw error;
+  })
+
+
+    
