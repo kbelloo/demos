042b968502078c66121136b2ab036c624f07e0ba
Khalid Bello <khalidbello279@gmail.com>
2021-04-11 14:57:04 +0100

    axes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-let margin = {top: 30, right: 30, bottom: 30, left: 30},
+let margin = {top: 30, right: 30, bottom: 150, left: 30},
     width = 960 - margin.left -margin.right, 
     height = 500 - margin.top - margin.bottom;
 
@@ -12,18 +12,49 @@ let svg = d3.select('#viz')
   .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 
+let x = d3.scaleBand()
+    .range([0, width])
+    .padding(0.1);
 
-  d3.csv('sales.csv', (d) => {
+
+let y = d3.scaleLinear()
+    .range([height, 0]);
+
+    
+d3.csv('sales.csv', (d) => {
 
     d.sales = +d.sales;
     return d;
 
   }).then((results) => {
 
-    console.log(results);
+
+      x.domain(results.map(d => d.flavors)); //mapping the flavors from csv file
+    
+    
+      y.domain([0, d3.max(results, d => d.sales)])
+        .nice();  //make the y-axis show 500 since it stopped at 495
+
+      svg.append('g')
+         .call(d3.axisLeft(y));
+
+      svg.append('g')
+         .attr("transform", `translate(0, ${height})`)
+         .call(d3.axisTop(x))
+         .selectAll("text")
+         .attr('x', x.bandwidth()/2)
+         .attr('y', 0)
+         .attr('dy', '.35em')
+         .attr('transform', 'rotate(90)')
+         .attr("text-anchor", "start"); 
+          
+            
+
 
   }).catch((error) => {
+
       throw error;
+
   })
 
 
